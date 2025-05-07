f531ba27b32c77b13b51c8441d8acf5f34f93ac0
MacBook Pro <hx101@duke.edu>
2025-05-07 21:30:06 +0800

    build: Changed all tool and resource functions to execute asynchronously

--- src/doris_mcp_server/server.py
+++ src/doris_mcp_server/server.py
@@ -1,5 +1,7 @@
-import sys
 import os
+import sys
+import asyncio
+from doris_mcp_server import config
 from doris_mcp_server.mcp_app import mcp
 from doris_mcp_server import config
 from doris_mcp_server.db import tools, DorisConnector
@@ -7,20 +9,19 @@ from doris_mcp_server.res import resources
 from doris_mcp_server.prompts import general_prompts, customize_prompts
 import traceback
 
-
 class MCPDorisServer:
 
     def __init__(self):
         self.server = mcp
 
 
-    def _test_db_connection(self):
+    async def _test_db_connection(self):
         """
         测试数据库连接是否成功。
         """
         try:
-            with DorisConnector() as conn:
-                result = conn.execute_query("SELECT 1")
+            async with DorisConnector() as conn:
+                result = await conn.execute_query("SELECT 1")
                 if result:
                     print("✅ Database connection successful.")
                 else:
@@ -30,29 +31,27 @@ class MCPDorisServer:
             raise e
 
 
-    def run(self):
+    async def run_async(self):
         """
         启动 MCP Server
         """
         try:
             print("🚀 Doris MCP Server is starting...")
-            self._test_db_connection()
-            self.server.run()
+            await self._test_db_connection()
+            await self.server.run()
         except Exception as e:
             print("🚨 Doris MCP Server failed to start.")
             print(f"Error: {e}")
             traceback.print_exc()
 
 
-def main():
-    # 如果提供了命令行参数，则设置为 DORIS_URL
+async def main():
     if len(sys.argv) > 1:
         os.environ["DORIS_URL"] = sys.argv[1]
-    
+
     config.init_config()
 
-    app = MCPDorisServer()
-    app.run()
+    server = MCPDorisServer()
+    await server.run_async()
 
-if __name__ == "__main__":
-    main()
+
